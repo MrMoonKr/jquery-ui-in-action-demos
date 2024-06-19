0d0c2a450d4e8f824054686ee6c6b89c96458c3c
Mr.Moon <MrMoonKr@gmail.com>
2024-06-19 11:44:37 +0900

    웹팩 기반 프로젝트로 구성 ...

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+import $ from 'jquery';
+import 'jquery-ui-dist/jquery-ui.js';
+//import 'jquery-ui-dist/jquery-ui.css';
+//import 'jquery-ui-dist/jquery-ui.theme.css';
+//import 'jquery-ui-dist/jquery-ui.structure.css';
+
+$( function() {
+    $( '#draggable' ).draggable();
+} );
